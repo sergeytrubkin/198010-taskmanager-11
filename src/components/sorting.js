9eef43a06f3a70ff36c90cc8f6e3f38c7eed2ed7
Трубкин Сергей <trubkinsa@gmail.com>
2020-04-05 15:48:29 +0300

    Разбивает один большой файл на модули и импортирует в точку входа

---
+++ src/components/sorting.js
@@ -0,0 +1,6 @@
+export const createSortingTemplate = () => `
+  <div class="board__filter-list">
+    <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
+    <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
+    <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+  </div>`;
