2c0d637973cf48d9f4639de3a101dc821de23eb9
Трубкин Сергей <trubkinsa@gmail.com>
2020-04-24 21:22:42 +0300

    Использует новые функции для отрисовки

--- src/main.js
+++ src/main.js
@@ -17,33 +17,34 @@ const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 const renderTask = (taskListElement, task) => {
   const onEditButtonClick = () => {
     taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-  }
+  };
 
   const onEditFormSubmit = (evt) => {
     evt.preventDefault();
     taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-  }
+  };
 
   const taskComponent = new TaskComponent(task);
-  const editButton = taskComponent.getElement.querySelector(`.card__btn--edit`);
+  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
   editButton.addEventListener(`click`, onEditButtonClick);
 
   const taskEditComponent = new TaskEditComponent(task);
   const editForm = taskEditComponent.getElement().querySelector(`form`);
   editForm.addEventListener(`submit`, onEditFormSubmit);
-
-  render(taskListElement, taskComponent.getElement());
+  render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
 const renderBoard = (boardComponent, tasks) => {
-  render(boardComponent.getElement(), new SortingComponent().getElement());
-  render(boardComponent.getElement(), new TasksComponent().getElement());
+  render(boardComponent.getElement(), new SortingComponent().getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
 
   const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
 
   let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
   tasks.slice(0, showingTasksCount)
-    .forEach((task) => renderTask(taskListElement, task));
+    .forEach((task) => {
+      renderTask(taskListElement, task);
+    });
 
   const loadMoreButtonComponent = new LoadMoreButtonComponent();
   render(boardComponent.getElement(), loadMoreButtonComponent.getElement());
@@ -70,5 +71,10 @@ const tasks = generateTasks(TASK_COUNT);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, new SiteMenuComponent());
-render(siteMainElement, new FilterComponent(filters));
+render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
+
+const boardComponent = new BoardComponent();
+
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+renderBoard(boardComponent, tasks);
